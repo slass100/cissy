020a8688e9c58c1cef86ba1aba0c89de97f9a6fd
slass100 <slass100@users.noreply.github.com>
2015-08-27 21:14:01 -0500

    basic tests

---
+++ test/test_in_out.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ANYFAIL=0;
+
+for file in test_in_out/test_*_input.txt
+do
+    TEST=`echo ${file} | sed 's/.*\/\(.*\)_input.txt/\1/'`
+    echo -n "Running ${TEST}:"
+    cat test_in_out/${TEST}_input.txt \
+	| ../src/cissy.exe `cat test_in_out/${TEST}_cmdargs.txt` \
+	> test_in_out/${TEST}_gen.txt
+    diff test_in_out/${TEST}_gen.txt test_in_out/${TEST}_output.txt > /dev/null
+    if [ $? -ne 0 ]; then
+	echo -e "\tFail";
+	ANYFAIL=1;
+    else
+	echo -e "\tPass";
+    fi
+done
+
+exit ${ANYFAIL}
